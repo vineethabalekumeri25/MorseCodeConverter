f08516a9e30fba3ea61082f3caf57d56ccb819e5
vineethabalekumeri25 <vineethab322@gmail.com>
2024-11-26 11:42:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# Morse Code Dictionary
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----', ',': '--..--',
+    '.': '.-.-.-', '?': '..--..', "'": '.----.', '!': '-.-.--',
+    '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.',
+    '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-',
+    '@': '.--.-.', ' ': '/'
+}
+
+def text_to_morse(text):
+    """Convert text to Morse code."""
+    morse_code = []
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code.append(MORSE_CODE_DICT[char])
+        else:
+            morse_code.append('')  # For unsupported characters, leave blank
+    return ' '.join(morse_code)
+
+# Main Program
+if __name__ == "__main__":
+    print("Text to Morse Code Converter")
+    user_input = input("Enter text to convert to Morse Code: ")
+    morse_output = text_to_morse(user_input)
+    print("\nMorse Code:")
+    print(morse_output)
